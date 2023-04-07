2454af89f65a48866f3aad13e89a6f848ee80230
NaveYadai <87646702+NAVEYADAI@users.noreply.github.com>
2023-04-07 08:28:37 +0300

    Initial commit

---
+++ src/main/java/com/example/medicinesspring/MedicinesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicinesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinesSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medicinesspring/MedicinesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicinesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
